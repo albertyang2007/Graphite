62bb3190a73f4e709bea95ba1f993c477fda8c85
eyaweiw <albert.yang@ericsson.com>
2014-01-13 15:18:08 +0800

    remove something...

--- src/main/java/org/albertyang2007/graphite/json/GraphiteJsonClient2.java
+++ src/main/java/org/albertyang2007/graphite/json/GraphiteJsonClient2.java
@@ -1,12 +1,3 @@
-/*------------------------------------------------------------------------------
- * COPYRIGHT Ericsson 2014
- *
- * The copyright to the computer program(s) herein is the property of
- * Ericsson Inc. The programs may be used and/or copied only with written
- * permission from Ericsson Inc. or in accordance with the terms and
- * conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
- *----------------------------------------------------------------------------*/
 package org.albertyang2007.graphite.json;
 
 import java.io.IOException;
